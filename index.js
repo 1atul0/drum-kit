45edd3663ceb81604f6762577da3801b2d505b1c
1atul0 <919atul@gmail.com>
2023-02-23 20:20:21 +0530

    bg pic added in css and evenlistner is added in js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 // alert("done");
 
 //add eventlistner to js
-document.querySelector("button").addEventListener("click",handleClick());
+//step 1
+// document.querySelector("button").addEventListener("click",handleClick);
+/**
+ if you use handleClick() instead of handleClick then handleClick() direct
+ ly called even without clicked.
+ */
 
-function handleClick()
+// function handleClick()
+// {
+//   alert("clicked");
+// }
+
+//other step using anonymous function
+// document.querySelector("button").addEventListener("click",function(){
+//   alert("clicked");
+// });
+// document.querySelectorAll("button")[2].addEventListener("click",function(){
+//   alert("all clicked");
+// });
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
 {
-  alert("clicked");
+  document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+    var audio = new Audio("sounds/tom-1.mp3");
+audio.play();
+
+  });
 }
